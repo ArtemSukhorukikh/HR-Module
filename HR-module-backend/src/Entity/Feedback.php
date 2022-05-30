b395faa9179d5b87f790d9859080477cd558a1df
nuviky <nuviky@gmail.com>
2022-05-30 21:19:58 +0300

    Создание отзыва

---
+++ HR-module-backend/src/Entity/Feedback.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FeedbackRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FeedbackRepository::class)
+ */
+class Feedback
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $estimation;
+
+    /**
+     * @ORM\Column(type="string", length=3000)
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getEstimation(): ?int
+    {
+        return $this->estimation;
+    }
+
+    public function setEstimation(int $estimation): self
+    {
+        $this->estimation = $estimation;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
