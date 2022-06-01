fcab3e8de0ab8177e8d4c1848952960ed72b74e2
nuviky <nuviky@gmail.com>
2022-06-01 23:10:45 +0300

    Дто отзывы исправления

--- HR-module-backend/src/Dto/Feedback/Request/FeedbackUserRequest.php
+++ HR-module-backend/src/Dto/Feedback/Request/FeedbackUserRequest.php
@@ -5,17 +5,18 @@ namespace App\Dto\Feedback\Request;
 use App\Dto\Feedback\FeedbackUserDTO;
 use App\Dto\Transformer\Request\AbstractRequestDTOTransformer;
 use App\Entity\Feedback;
-use App\Entity\User;
-use App\Entity\EducationalResources;
 
 class FeedbackUserRequest extends AbstractRequestDTOTransformer
 {
     /**
      * @param FeedbackUserDTO $object
      */
-    public function transformToObject($object)
+    public function transformToObject($object): Feedback
     {
         $data = new Feedback();
+        $data->setEstimation($object->feedbackDTO->estimation);
+        $data->setDate($object->feedbackDTO->date);
+        $data->setNote($object->feedbackDTO->note);
         return $data;
     }
 }
--- HR-module-backend/src/Dto/Feedback/Response/FeedbackUserResponse.php
+++ HR-module-backend/src/Dto/Feedback/Response/FeedbackUserResponse.php
@@ -3,6 +3,7 @@
 namespace App\Dto\Feedback\Response;
 
 
+use App\Dto\Feedback\FeedbackDTO;
 use App\Dto\Feedback\FeedbackUserDTO;
 use App\Entity\Feedback;
 use App\Entity\User;
@@ -12,10 +13,16 @@ use App\Dto\Transformer\Response\AbstractResponceDTOTransformer;
 class FeedbackUserResponse  extends AbstractResponceDTOTransformer
 {
     /**
-     * @param Feedback $feedback
+     * @param Feedback $object
      */
-    public function transformFromObject($feedback): FeedbackUserDTO
+    public function transformFromObject($object): FeedbackUserDTO
     {
-
+        $feedback = new FeedbackDTO();
+        $feedback->estimation = $object->getEstimation();
+        $feedback->date = $object->getDate();
+        $feedback->note = $object->getNote();
+        $dto = new FeedbackUserDTO($feedback);
+        $dto->userId = $object->getAuthon()->getId();
+        $dto->educationalResourcesId = $object->getEducationalResources()->getId();
     }
 }
