d89009ae4b569aad82f08780535a356a4bcdb900
nuviky <73002573+nuviky@users.noreply.github.com>
2022-06-14 17:30:04 +0300

    Исправления заявок

--- HR-module-frontend/src/views/Application/DepartmentApplication.vue
+++ HR-module-frontend/src/views/Application/DepartmentApplication.vue
@@ -131,6 +131,7 @@
               <div class="col-lg-12 col-xl-11">
                 <div v-for="item in applicationPOPT" v-bind:key="item">
                   <div class="card text-black" style="border-radius: 25px;" v-if="item.status === status" >
+                    <div class="card-title col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1"> {{item.name}} </div>
                     <div class="card-body p-md-5" >
                       <div class="row justify-content-center">
                         <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
@@ -203,6 +204,7 @@
             <div class="row d-flex justify-content-center align-items-center h-100">
               <div class="col-lg-12 col-xl-11" v-for="item in applicationPOT" v-bind:key="item">
                 <div class="card text-black" style="border-radius: 25px;" v-if="item.status === status">
+                  <div class="card-title col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1"> {{item.name}} </div>
                   <div class="card-body p-md-5" >
                     <div class="row justify-content-center">
                       <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
